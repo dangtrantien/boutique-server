a3763f8793d2df19c6d5bd998a809bcdf70fa235
Đặng Trần Tiến <tiendtfx22706@funix.edu.vn>
2023-11-01 12:07:08 +0700

    update env

--- app.js
+++ app.js
@@ -56,8 +56,6 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: [
-      // 'http://192.168.1.107:3000',
-      // 'http://192.168.1.107:3001',
       'https://boutique-client.onrender.com',
       'https://boutique-admin.onrender.com',
     ],
